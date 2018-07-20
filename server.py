b546b65bf70be2fe14fbf76a676640360bcc86b8
AndreyAD1 <arakelyants.andrey@yandex.ru>
2018-07-20 17:11:28 +0300

    Make the first working version of the site.

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from typograph import launch_typograph
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def form():
-    return render_template('form.html')
+@app.route('/', methods=['GET', 'POST'])
+def form(edited_text=None):
+    if request.method == 'POST':
+        user_text = request.form['text']
+        edited_text = launch_typograph(user_text)
+        print(edited_text)
+    return render_template('form.html', edited_text=edited_text)
 
 
 if __name__ == "__main__":
--- typograph.py
+++ typograph.py
@@ -1,31 +1,21 @@
-import re
 import regex
-
-
-# замена кавычек ' и " на « »
-# в нужных местах заменить дефисы на тире
-# замена дефисов на короткое тире в номерах телефонов
-# связывание чисел с последующими словами неразрывным пробелом
-# удаление лишних пробелов и переносов строк
-# связывание союзов и любых слов из 1-2 символов с последующими словами
 
 
 def get_text():
     return '''
-    - Что ты говоришь? Рим - столица Италии. Человек-пароход. Звоните: 228-36-956 -6 Петя-1 2018 год 19   лет
-    Falcon-9 совершил свой первый полёт.\n2020 год
+    <p>- Что ты говоришь?</p> Рим - столица Италии. Человек-пароход. Звоните: 228-36-956 -6 Петя-1 2018 год 19   лет
+    <b>Falcon-9</b> совершил свой первый полёт и он был успешен.\n2020 год
     2019 год.
     
-    \n
-    end
-    start
+    в 1989 году
+    не очень-то круто
     '''
 
 
 def fix_left_quotes(string):
     left_quote_pattern = r'[\'|"](?=\w)'
     html_code_of_left_quote = '&#171'
-    fixed_left_quotes = re.sub(
+    fixed_left_quotes = regex.sub(
         left_quote_pattern,
         html_code_of_left_quote,
         string
@@ -36,7 +26,7 @@ def fix_left_quotes(string):
 def fix_right_quotes(string):
     right_quote_pattern = r'(?<=\w)[\'|"]'
     html_code_of_right_quote = '&#187'
-    fixed_right_quotes = re.sub(
+    fixed_right_quotes = regex.sub(
         right_quote_pattern,
         html_code_of_right_quote,
         string
@@ -47,14 +37,14 @@ def fix_right_quotes(string):
 def substitute_hyphens_for_dashes(string):
     dash_pattern = r'(?<=\s)-|-(?=\s)'
     dash_html_code = '&#8211'
-    fixed_dashes = re.sub(dash_pattern, dash_html_code, string)
+    fixed_dashes = regex.sub(dash_pattern, dash_html_code, string)
     return fixed_dashes
 
 
 def substitute_hyphens_for_short_dashes(string):
     short_dash_pattern = r'(?<=\d)-(?=\d)'
     html_code_of_short_dash = '&#8210'
-    fixed_short_dashes = re.sub(
+    fixed_short_dashes = regex.sub(
         short_dash_pattern,
         html_code_of_short_dash,
         string
@@ -62,8 +52,9 @@ def substitute_hyphens_for_short_dashes(string):
     return fixed_short_dashes
 
 
-def get_nonbreaking_space(match_object):
-    if re.match(r'\s', match_object.group(1)):
+def get_nonbreaking_space_for_numbers(match_object):
+    symbol_before_number = match_object.group(1)
+    if regex.match(r'\s', symbol_before_number):
         str_without_end_space = match_object.group(0).rstrip()
         nonbreaking_space = '&#160'
         str_with_nonbreaking_space = str_without_end_space + nonbreaking_space
@@ -75,15 +66,15 @@ def add_nonbreaking_spaces(string):
     search_pattern = r'(\D)(\d+\s)(?=\p{L})'
     text_with_nonbreaking_spaces = regex.sub(
         search_pattern,
-        get_nonbreaking_space,
+        get_nonbreaking_space_for_numbers,
         string
     )
     return text_with_nonbreaking_spaces
 
 
 def delete_redundant_spaces(string):
-    redundant_space_pattern = r'[ \t\r\f\b][ \t\r\f\b]+'
-    text_without_redundant_spaces = re.sub(
+    redundant_space_pattern = r'  +'
+    text_without_redundant_spaces = regex.sub(
         redundant_space_pattern,
         ' ',
         string
@@ -92,23 +83,46 @@ def delete_redundant_spaces(string):
 
 
 def delete_empty_lines(string):
-    empty_line_pattern = r'\n\n+'
-    text_without_empty_lines = re.sub(
+    empty_line_pattern = r'\s\s+'
+    text_without_empty_lines = regex.sub(
         empty_line_pattern,
-        '!',
+        '\n',
         string
     )
     return text_without_empty_lines
 
 
+def get_nonbreaking_space_for_short_words(match_object):
+    matched_str = match_object.group(0)
+    nonbreaking_space = '&#160'
+    str_with_nonbreaking_space = matched_str.rstrip() + nonbreaking_space
+    return str_with_nonbreaking_space
+
+
+def bind_short_words_together(string):
+    short_words_pattern = r'\s\p{L}{1,2} '
+    text_with_bound_short_words = regex.sub(
+        short_words_pattern,
+        get_nonbreaking_space_for_short_words,
+        # '!',
+        string
+    )
+    return text_with_bound_short_words
+
+
+def launch_typograph(content):
+    content = delete_redundant_spaces(content)
+    content = add_nonbreaking_spaces(content)
+    content = fix_left_quotes(content)
+    content = fix_right_quotes(content)
+    content = substitute_hyphens_for_dashes(content)
+    content = substitute_hyphens_for_short_dashes(content)
+    content = delete_empty_lines(content)
+    content = bind_short_words_together(content)
+    return content
+
+
 if __name__ == '__main__':
     text = get_text()
-    text = delete_redundant_spaces(text)
-    text = add_nonbreaking_spaces(text)
-    text = fix_left_quotes(text)
-    text = fix_right_quotes(text)
-    text = substitute_hyphens_for_dashes(text)
-    text = substitute_hyphens_for_short_dashes(text)
-
-    text = delete_empty_lines(text)
+    text = launch_typograph(text)
     print(text)
