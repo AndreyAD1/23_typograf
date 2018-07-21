0a6be6b62b3b1345bc04919f46162729a13d1bf2
AndreyAD1 <arakelyants.andrey@yandex.ru>
2018-07-21 15:43:09 +0300

    Delete excess functions.

--- typograph.py
+++ typograph.py
@@ -1,18 +1,6 @@
 import regex
 
 
-def get_text():
-    return '''
-    <p>- Что ты говоришь?</p> Рим - столица Италии. Человек-пароход. Звоните: 228-36-956 -6 Петя-1 2018 год 19   лет
-    <b>Falcon-9 совершил свой первый полёт и он был успешен.\n2020 год
-    2019 год.
-    
-    в 1989 году
-    не очень-то круто
-    Ку ка ре ку
-    '''
-
-
 def fix_left_quotes(string):
     left_quote_pattern = r'[\'|"](?=\w)'
     html_code_of_left_quote = '&#171'
@@ -36,7 +24,7 @@ def fix_right_quotes(string):
 
 
 def substitute_hyphens_for_dashes(string):
-    dash_pattern = r'(?<=\s)-|-(?=\s)'
+    dash_pattern = r'(?<=\s)-(?=\s)'
     dash_html_code = '&#8211'
     fixed_dashes = regex.sub(dash_pattern, dash_html_code, string)
     return fixed_dashes
@@ -53,21 +41,11 @@ def substitute_hyphens_for_short_dashes(string):
     return fixed_short_dashes
 
 
-def get_nonbreaking_space_for_numbers(match_object):
-    symbol_before_number = match_object.group(1)
-    if regex.match(r'\s', symbol_before_number):
-        str_without_end_space = match_object.group(0).rstrip()
-        nonbreaking_space = '&#160'
-        str_with_nonbreaking_space = str_without_end_space + nonbreaking_space
-        return str_with_nonbreaking_space
-    return match_object.group(0)
-
-
-def add_nonbreaking_spaces(string):
-    search_pattern = r'(\D)(\d+\s)(?=\p{L})'
+def bind_numbers_with_words(string):
+    search_pattern = r'((\s|\A)\d+)(\s)(?=\p{L})'
     text_with_nonbreaking_spaces = regex.sub(
         search_pattern,
-        get_nonbreaking_space_for_numbers,
+        r'\1&#160',
         string
     )
     return text_with_nonbreaking_spaces
@@ -93,19 +71,11 @@ def delete_empty_lines(string):
     return text_without_empty_lines
 
 
-def get_nonbreaking_space_for_short_words(match_object):
-    matched_str = match_object.group(0)
-    nonbreaking_space = '&#160'
-    str_with_nonbreaking_space = matched_str.rstrip() + nonbreaking_space
-    return str_with_nonbreaking_space
-
-
 def bind_short_words_together(string):
-    short_words_pattern = r'(?<!-)\b\p{L}{1,2} (?=\p{L})'
+    short_words_pattern = r'(?<!-)(\b\p{L}{1,2})( )(?=\p{L})'
     text_with_bound_short_words = regex.sub(
         short_words_pattern,
-        get_nonbreaking_space_for_short_words,
-        # '!',
+        r'\1&#160',
         string
     )
     return text_with_bound_short_words
@@ -113,7 +83,7 @@ def bind_short_words_together(string):
 
 def launch_typograph(content):
     content = delete_redundant_spaces(content)
-    content = add_nonbreaking_spaces(content)
+    content = bind_numbers_with_words(content)
     content = fix_left_quotes(content)
     content = fix_right_quotes(content)
     content = substitute_hyphens_for_dashes(content)
@@ -124,6 +94,6 @@ def launch_typograph(content):
 
 
 if __name__ == '__main__':
-    text = get_text()
+    text = '2016 ujl. Погоди-ка. Он - сумасшедший! 1984 год. Фалкон-9 взлетел 289 ! Я не ем много.'
     text = launch_typograph(text)
     print(text)
